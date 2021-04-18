111e6a024ed7d28123a3d363707646f2c64ca7b9
Connor McCarthy <mccarthy.connor.james@gmail.com>
2021-04-18 12:33:39 -0600

    update tests

--- tests/test_garm2vec.py
+++ tests/test_garm2vec.py
@@ -6,7 +6,15 @@ from garm2vec.constants import EMBEDDING_DIMS
 def test_garm2vec_runs():
     image_path = "/Users/connor/workspace/garm2vec/data/images/1163.jpg"
     description = "Turtle Check Men Navy Blue Shirt"
+    inputs = [image_path, description]
     g2v = Garm2Vec()
-    vector = g2v.get_vector(image_path, description)
+    vector = g2v.get_one(inputs)
     assert isinstance(vector, np.ndarray)
     assert vector.shape == (EMBEDDING_DIMS,)
+
+    vectors = g2v.get_many([inputs, inputs])
+    assert isinstance(vectors, np.ndarray)
+    assert vector.shape[-1] == EMBEDDING_DIMS
+
+    g2v.plot_one(inputs)
+    g2v.plot_many([inputs, inputs])
